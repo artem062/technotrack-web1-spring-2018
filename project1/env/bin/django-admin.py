7f093ae4253075a41ac4d209f9e88a95b239cd4b
Voronov Artyom <artem_062@mail.ru>
2018-03-20 01:09:00 +0300

    fix env

--- project1/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/artem/web-projects/project1/env/bin/python2
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- project1/src/core/urls.py
+++ project1/src/core/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^register$', register, name='register'),
     url(r'^login$', login, name='login'),
     url(r'^logout$', logout, name='logout'),
-    url(r'^$', page),
+    url(r'^$', page, name='main'),
 ]
