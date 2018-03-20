600252868f165d010b43c6a85f57c6e83e25c985
Voronov Artyom <artem_062@mail.ru>
2018-03-20 19:03:24 +0300

    add_quest+add_answ

--- project1/src/questions/urls.py
+++ project1/src/questions/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
-from questions.views import question_detail, questions_list
+from questions.views import question_detail, questions_list, question_add, answer_add
 
 urlpatterns = [
 
     url(r'^(?P<pk>\d+)/$', question_detail, name='question_detail'),
     url(r'^$', questions_list, name='questions_list'),
+    url(r'^add/(?P<pk>\d+)/$', question_add, name='questions_add'),
+    url(r'^add_answer/(?P<pk>\d+)/$', answer_add, name='answer_add'),
+
 ]
--- project1/src/questions/views.py
+++ project1/src/questions/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse
 from .models import Question, Answer
+from categories.models import Category
 
 
 def questions_list(request):
@@ -31,3 +32,20 @@ def answer_detail(request, pk=None):
         'question': answer.question
     }
     return render(request, 'questions/answer_page.html', context)
+
+
+def question_add(request, pk=None):
+
+    context = {
+        'category': Category.objects.get(id=pk),
+    }
+
+    return render(request, 'questions/question_add.html', context)
+
+
+def answer_add(request, pk=None):
+
+    context = {
+        'question': Question.objects.get(id=pk),
+    }
+    return render(request, 'questions/answer_add.html', context)
